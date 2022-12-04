33101d8c65a66ae91ca7e11efb77fd17b8c2bab6
yo-ichi <i10566@nara.kosen-ac.jp>
2022-12-04 21:35:02 +0900

    Vue Routingを設定

---
+++ src/views/TutorialView.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const user = router.currentRoute.value.params["user"];
+const repository = router.currentRoute.value.params["repository"];
+</script>
+
+<template>
+  <div class="tutorial">
+    <h1>Tutorial page</h1>
+    <p>{{ user }}</p>
+    <p>{{ repository }}</p>
+  </div>
+</template>
+
+<style></style>
